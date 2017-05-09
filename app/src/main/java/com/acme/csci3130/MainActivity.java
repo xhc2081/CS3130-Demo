76639fadddb0ff5cf336bc9becc5a5aab82ee7da
xhc2081 <xhc2081@163.com>
2017-05-09 18:16:04 -0300

    add one bottum and a text

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,11 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.style.TextAppearanceSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        final TextView helloText = (TextView) findViewById(R.id.helloText);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                String s = editText.getText().toString().trim();
+                helloText.setText(s);
+
+            }
+        });
     }
 }
